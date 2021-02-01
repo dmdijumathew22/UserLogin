63cec4213b4b064496c99614d5232e64fdfb703d
dmdijumathew22 <dmdijumathew22@gmail.com>
2021-01-31 22:04:52 -0500

    commit

--- src/main/java/com/hcl/simplilean/UserLogin/entity/UserEntity.java
+++ src/main/java/com/hcl/simplilean/UserLogin/entity/UserEntity.java
@@ -10,44 +10,42 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Data;
+
 @Table(name = "User")
 @Entity
 @Data
-public class UserEntity implements Serializable{
-	
-	/** Entity Class.
-	 * getters and setters generated using project lombok //@Data annotation
+public class UserEntity implements Serializable {
+
+	/**
+	 * Entity Class. getters and setters generated using project lombok //@Data
+	 * annotation
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
 
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private long id;
-	
-	@Column(name = "user_id",unique=true)
-	
+
+	@Column(name = "user_id", unique = true)
+
 	private String userid;
-	
+
 	@Column(name = "first_name")
-	
+
 	private String firstName;
-	
+
 	@Column(name = "last_name")
-	
+
 	private String lastName;
-	
+
 	@Column(name = "pwd")
-	
+
 	private String pwd;
-	
-	@Column(name = "email",unique=true)
-	
+
+	@Column(name = "email", unique = true)
+
 	private String email;
-	
-	
-	
 
 }
